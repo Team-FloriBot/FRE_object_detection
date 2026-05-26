08e9224a340660fb60f7378f0e31fba478c31b29
astark146 <aaronstark1406@gmail.com>
2026-05-26 12:58:28 +0200

    fix: update RGBD subscription topic path to match new structure

--- ros2_detection/ros2_detection/detector_node.py
+++ ros2_detection/ros2_detection/detector_node.py
@@ -136,7 +136,7 @@ class DetectionNode(Node):
 
         self.sub = self.create_subscription(
             RGBD,
-            "/sensors/realsense_front/rgbd/image",
+            "/sensors/realsense_front/rgbd",
             self.realsense_callback,
             10
         )
